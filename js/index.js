503fce5eff994fb55ba907c57d07bc6179dedab1
cristian021195 <cristiangramajo015@gmail.com>
2022-07-05 00:29:15 -0300

    se agregaron aclaraciones y comentarios

--- js/index.js
+++ js/index.js
@@ -8,16 +8,16 @@ const ActualizarResultado = Debounce((text, long)=>{//Debounce, se ejecuta una s
 }, 500)
 
 $debounceInput.addEventListener('input', (e)=>{
-    ActualizarResultado(e.target.value, e.target.value.length)
+    ActualizarResultado(e.target.value, e.target.value.length)//los argumentos estos, pasan a ser del return de debounce, ¿por que?
 })
 
 
 function Debounce(callback, delay = 1000){    
-    let timer;
-    return (...args) => {
-        clearTimeout(timer)
+    let timer;  
+    return (...args) => {//recibe los dos argumentos en forma de array de ActualizarResultado, el e.target.value y length y luego hace el spread ['a',1] ... ['asd',3]
+        timer && clearTimeout(timer)
         timer = setTimeout(() => {
-            callback(...args)
+            callback(...args)//recibe los dos argumentos en forma de array de ActualizarResultado, el e.target.value y length y luego hace el spread = ['asd',3]
         }, delay);
     }
 }
