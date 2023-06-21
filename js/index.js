fc79a83602c1e3331aa9f62c43084ac6e9da1a5a
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2023-06-20 22:14:21 -0300

    Cambios y agregado ejemplo con lodash

--- js/index.js
+++ js/index.js
@@ -2,10 +2,20 @@ const $debounceInput = document.getElementById('debounce-input');
 const $resultado = document.getElementById('resultado');
 const $cantidadLetras = document.getElementById('letras');
 
+const $lodash_form = document.getElementById('lodash-form');
+const $lodash_input = document.getElementById('lodash-input');
+const $lodash_resultado = document.getElementById('lodash-resultado');
+const $lodash_letras = document.getElementById('lodash-letras');
+
 const ActualizarResultado = Debounce((text, long)=>{//Debounce, se ejecuta una sola vez, y ActualizarResultado pasa a tener lo mismo que Debounce
     $resultado.textContent = text;
     $cantidadLetras.textContent = long;
 }, 500)
+
+$lodash_input.addEventListener('input', _.debounce((e)=>{
+    $lodash_resultado.textContent = e.target.value;
+    $lodash_letras.textContent = e.target.value.length;
+}, 1000))
 
 $debounceInput.addEventListener('input', (e)=>{
     ActualizarResultado(e.target.value, e.target.value.length)//los argumentos estos, pasan a ser del return de debounce, ¿por que?
