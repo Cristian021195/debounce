e197157f6b8f5ded6a19eeca2c1fd879bbdc97fe
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2023-08-28 11:33:28 -0300

    Update index.js
    cambios para delay dinamico segun entrada

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 const $debounceInput = document.getElementById('debounce-input');
 const $resultado = document.getElementById('resultado');
 const $cantidadLetras = document.getElementById('letras');
+const $valores = document.getElementById('valores');
 
 const $lodash_form = document.getElementById('lodash-form');
 const $lodash_input = document.getElementById('lodash-input');
 const $lodash_resultado = document.getElementById('lodash-resultado');
 const $lodash_letras = document.getElementById('lodash-letras');
+let select_time = 1000;
+$valores.addEventListener('change', (e)=>{
+    select_time = parseInt(e.target.value);
+    console.log("cambia st:",select_time);
+})
 
 const ActualizarResultado = Debounce((text, long)=>{//Debounce, se ejecuta una sola vez, y ActualizarResultado pasa a tener lo mismo que Debounce
     $resultado.textContent = text;
     $cantidadLetras.textContent = long;
-}, 500)
+},select_time)
 
 $lodash_input.addEventListener('input', _.debounce((e)=>{
     $lodash_resultado.textContent = e.target.value;
@@ -18,18 +24,17 @@ $lodash_input.addEventListener('input', _.debounce((e)=>{
 }, 1000))
 
 $debounceInput.addEventListener('input', (e)=>{
-    ActualizarResultado(e.target.value, e.target.value.length)//los argumentos estos, pasan a ser del return de debounce, ¿por que?
+    ActualizarResultado(e.target.value, e.target.value.length, select_time)//los argumentos estos, pasan a ser del return de debounce, ¿por que?
 })
 
-
-function Debounce(callback, delay = 1000){    
-    let timer;  
+function Debounce(callback, delay){    
+    let timer;    
     return (...args) => {//recibe los dos argumentos en forma de array de ActualizarResultado, el e.target.value y length y luego hace el spread ['a',1] ... ['asd',3]
-        timer && clearTimeout(timer)
+        timer && clearTimeout(timer);
+        console.log();
         timer = setTimeout(() => {
             callback(...args)//recibe los dos argumentos en forma de array de ActualizarResultado, el e.target.value y length y luego hace el spread = ['asd',3]
-        }, delay);
+        }, args[args.length - 1]);
     }
+    
 }
-
-
